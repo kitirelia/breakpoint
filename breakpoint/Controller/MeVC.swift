b383a82180ba496bd1c2e948fb2087fa83ff2130
kitiwat chanluthin <aurelia@kitiwats-MacBook-Pro.local>
2017-08-21 14:10:56 +0700

    Building MeVC

---
+++ breakpoint/Controller/MeVC.swift
@@ -0,0 +1,27 @@
+//
+//  MeVC.swift
+//  breakpoint
+//
+//  Created by kitiwat chanluthin on 8/21/17.
+//  Copyright © 2017 kitiwat chanluthin. All rights reserved.
+//
+
+import UIKit
+
+class MeVC: UIViewController {
+
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLbl: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func signOutBtnPressed(_ sender: Any) {
+        //Sign out code
+        
+    }
+    
+
+}
